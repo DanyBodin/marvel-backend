f72e58a13bcaabceafbe4b88f586afabcdf9a3c7
DANY <dany@macbook-pro-de-dany.home>
2021-11-21 21:42:21 +0100

    deploy

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 const express = require("express");
 const formidableMiddleware = require("express-formidable");
-const mongoose = require("mongoose");
+const axios = require("axios");
+
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(formidableMiddleware());
 app.use(cors());
-mongoose.connect("mongodb://localhost/");
 
-app.get("/comics", (req, res) => {});
-
-app.get("/characters", (req, res) => {});
-
-app.get("/", (req, res) => {
-  res.send("Welcome to MarvelWorld");
+app.get("/comics", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `${process.env.SERVER_URI}/comics?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-app.listen(3000, () => {
+app.get("/characters", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `${process.env.SERVER_URI}/characters?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.get("/comics/charactersid", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `${process.env.SERVER_URI}/comics/characters:id?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.listen(process.env.PORT, () => {
   console.log("server up and running");
 });
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const express = require("express");
-
 const router = express.Router();
 const User = require("../models/User");
 
-module.exports(router);
+module.exports = router;
